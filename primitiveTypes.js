7a640a6f19473e0de98b2be7e9ea0085ff76af84
gaurav-nickelfox <gaurav.panday@nickelfox.com>
2022-12-06 10:17:05 +0530

    chore(ts/setup):basic setup for ts project learning

---
+++ primitiveTypes.js
@@ -0,0 +1,3 @@
+// Defining basics of using TS with Primitive Data types
+// Documentaion link
+// https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#the-primitives-string-number-and-boolean
